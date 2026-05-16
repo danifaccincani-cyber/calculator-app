cf3c08efc24b41eac56679564db88654560e9593
DanieleFaccincaniScuola <dani.faccincani@gmail.com>
2026-05-17 01:06:53 +0200

    Initialize main window

---
+++ calculator.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+window.geometry("600x600")
+window.title("calculator App")
+window.resizable(False,False)
+
+if __name__ == "__main__":
+    window.mainloop()    
